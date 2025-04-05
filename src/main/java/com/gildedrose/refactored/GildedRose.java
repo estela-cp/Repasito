352d42b54256f8bbe839afc405dbfd303f9a7d12
estelaCP <estela.cp06@icloud.com>
2025-04-05 11:43:27 +0200

    Todos los if mergeados

--- src/main/java/com/gildedrose/refactored/GildedRose.java
+++ src/main/java/com/gildedrose/refactored/GildedRose.java
@@ -14,27 +14,24 @@ class GildedRose {
     for (int i = 0; i < items.length; i++) {
       if (!items[i].name.equals(AGED_BRIE)
               && !items[i].name.equals(BACKSTAGE_PASSES)) {
-        if (items[i].quality > 0) {
-          if (!items[i].name.equals(SULFURAS)) {
+        if (items[i].quality > 0 && !items[i].name.equals(SULFURAS)) {
             items[i].quality = items[i].quality - 1;
           }
-        }
+
       } else {
         if (items[i].quality < 50) {
           items[i].quality++;
 
           if (items[i].name.equals(BACKSTAGE_PASSES)) {
-            if (items[i].sellIn < 11) {
-              if (items[i].quality < 50) {
+            if (items[i].sellIn < 11 && items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
               }
-            }
 
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
+
+            if (items[i].sellIn < 6 && items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
               }
-            }
+
           }
         }
       }
@@ -46,11 +43,10 @@ class GildedRose {
       if (items[i].sellIn < 0) {
         if (!items[i].name.equals(AGED_BRIE)) {
           if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-            if (items[i].quality > 0) {
-              if (!items[i].name.equals(SULFURAS)) {
+            if (items[i].quality > 0 && !items[i].name.equals(SULFURAS)) {
                 items[i].quality = items[i].quality - 1;
               }
-            }
+
           } else {
             items[i].quality = 0;
           }
